0e558fa0dfd2fb3ef63efe005de6fd9b0d8d8440
埋豆 <caocao0103@163.com>
2020-08-20 17:51:44 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from '../src/components/m-header/m-header'
+import Tab from '../src/components/tab/tab'
 
 export default {
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
-
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
   @import 'common/stylus/variable.styl'
 
-  #app
-    color: $color-theme
+  // #app
+  //   color: $color-theme
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Recommend from '@/components/recommend/recommend';
+import Singer from '@/components/singer/singer';
+import Rank from '@/components/rank/rank';
+import Search from '@/components/search/search';
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 根路径，选中状态
+  {
+    path: '/',
+    redirect: '/recommend',
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/rank',
+    component: Rank
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+
   // {
   //   path: '/',
   //   name: 'Home',
